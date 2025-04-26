837df7b7a31178537e596b52921e747ba4de92ce
NA980593 <90323608+NA980593@users.noreply.github.com>
2025-04-26 17:55:33 -0400

    a lot of stuff

--- scripts/book_ui.gd
+++ scripts/book_ui.gd
@@ -1,5 +1,15 @@
 extends Control
 
+var info = GameData.manual_dict
+
+@onready var label: Label = $HBoxContainer/Label
+
+@onready var button_holder: VBoxContainer = $HBoxContainer/ButtonHolder
+
+func _ready() -> void:
+	for key in info.keys():
+		add_button(key)
+
 func open_book():
 	visible = true
 
@@ -8,3 +18,12 @@ func close_book():
 
 func _on_button_pressed() -> void:
 	close_book()
+
+func add_button(text):
+	var button = Button.new()  
+	button.text = text    
+	button.pressed.connect(button_pressed.bind(info[text]))
+	button_holder.add_child(button)
+
+func button_pressed(data):
+	label.text = data
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,11 +1,23 @@
 extends Node
 
+@onready var patient_holder: Node2D = $PatientHolder
+
+@onready var book_ui: Control = $BookUI
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+var patient = preload("res://scenes/patient.tscn")
+
 func next_patient():
-	print("next patient")
+	spawn_patient()
 
 func open_book():
-	$BookUI.open_book()
+	book_ui.open_book()
 
+func spawn_patient():
+	var p = patient.instantiate()
+	patient_holder.add_child(p)
+	animation_player.play("patient_walk")
 
 func _on_book_open_book() -> void:
 	open_book()
